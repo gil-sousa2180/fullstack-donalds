27f493565263113866b85f98db11d0066ee90599
gil-sousa2180 <gilvani.ss@hotmail.com>
2025-02-20 21:54:54 -0300

    feat: add shadcn setup add componests

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const Products = () => {
+  return (
+    <div className="p-5 border bg-white border-green-200 roundel-xl">
+      <h1 className="text-blue-500">product page</h1>
+      <Button>FSD 2025</Button>
+      <Input placeholder="Vou terminar essa bagaça fi" />
+    </div>
+  );
+};
+
+export default Products;
