af88b903edc1b4316459c6ab17aa8f472279cd89
gil-sousa2180 <gilvani.ss@hotmail.com>
2025-02-23 14:52:59 -0300

    feat: ensure same products can be addded more than once to cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -31,7 +31,21 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     };
 
     const addProduct = (product: CartProduct) =>{
-        setProducts((prev) => [...prev, product]);
+        const productIsAlredyOnTheCart = products.some(prevProduct => prevProduct.id === product.id)
+        if (!productIsAlredyOnTheCart) {
+            return setProducts((prev) => [...products, product]);
+        }
+        setProducts(prevProducts => {
+            return prevProducts.map(prevProduct => {
+                if (prevProduct.id === product.id) {
+                    return {
+                        ...prevProduct,
+                        quantity: prevProduct.quantity + product.quantity
+                    }
+                }
+                return prevProduct
+            });
+        });
     };
 
     return (
