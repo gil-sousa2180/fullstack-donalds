91142494913792844be76f95d0c42d10e0136abf
gil-sousa2180 <gilvani.ss@hotmail.com>
2025-02-28 23:00:45 -0300

    feat: add initial firish order dialog components y create add helpers cpf validicion

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -1,38 +1,54 @@
-import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
-import { useContext } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useContext, useState } from "react";
 import { CartContext } from "../../contexts/cart";
 import CartProductItem from "../../components/cart-product-item";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { formatCurrency } from "@/format-currency";
+import { Button } from "@/components/ui/button";
+import FirishOrderDialog from "../../components/firish-order-dialog";
 
 const CartSheet = () => {
-    const { isOpen, toggleCart, products, total } = useContext(CartContext);
-    return (  
-        <Sheet open={isOpen} onOpenChange={toggleCart}>
-            <SheetContent className="w-[70%]">
-                    <SheetHeader>
-                    <SheetTitle className="text-left">Sacola</SheetTitle>
-                    </SheetHeader>
-                    <div className="flex h-full flex-col py-5">
-                    <div className="flex-auto">
-                    {products.map(product => (
-                        <CartProductItem key={product.id} product={product} />
-                    ))}
-                    </div>
-                    <Card className="mb-6">
-                        <CardContent className="p-5">
-                            <div className="flex justify-between">
-                                <p className="text-sm text-muted-foreground">Total</p>
-                                <p className="font-semibold text-sm">{formatCurrency(total)}</p>
-                            </div>
-                        </CardContent>
-                    </Card>
-                    <Button className="w-full rounded-full">Finalizar pedido</Button>
-                    </div>
-            </SheetContent>
-        </Sheet>
-    );
-}
- 
+  const [finishOrderDialogIsOpen, setFinishOrderDialogIsOpen] = useState(false);
+  const { isOpen, toggleCart, products, total } = useContext(CartContext);
+  return (
+    <Sheet open={isOpen} onOpenChange={toggleCart}>
+      <SheetContent className="w-[70%]">
+        <SheetHeader>
+          <SheetTitle className="text-left">Sacola</SheetTitle>
+        </SheetHeader>
+        <div className="flex h-full flex-col py-5">
+          <div className="flex-auto">
+            {products.map((product) => (
+              <CartProductItem key={product.id} product={product} />
+            ))}
+          </div>
+          <Card className="mb-6">
+            <CardContent className="p-5">
+              <div className="flex justify-between">
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="text-sm font-semibold">{formatCurrency(total)}</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Button
+            className="w-full rounded-full"
+            onClick={() => setFinishOrderDialogIsOpen(true)}
+          >
+            Finalizar pedido
+          </Button>
+          <FirishOrderDialog
+            open={finishOrderDialogIsOpen}
+            onOpenChange={setFinishOrderDialogIsOpen}
+          />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
 export default CartSheet;
--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -6,41 +6,53 @@ import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useContext } from "react";
 
 interface CartItemProps {
-    product: CartProduct;
-
+  product: CartProduct;
 }
 
-const CartProductItem = ({product}: CartItemProps) => {
-   const { decreaseProductQuantity, increaseProductQuantity, removeProduct} = useContext(CartContext);
-    return ( 
-       <div className="flex items-center justify-between">
-            <div className=" flex items-center gap-3">
-         
-             <div className="relative h-20 w-20 rounded-xl bg-gray-100">
-                <Image src={product.imageUrl} alt={product.name} fill /> 
-             </div> 
-             <div className="space-y-1">
-                <p className="max-w-[90%] truncate text-ellipsis text-xs">{product.name}</p>
-                <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
-                <div className="flex items-center gap-1 text-center">
-                   <Button className="h-7 w-7 rounded-lg" variant="outline" 
-                   onClick={() => decreaseProductQuantity(product.id)}>
-                     <ChevronLeftIcon />
-                   </Button>
-                   <p className="w-6 text-xs">{product.quantity}</p>
-                   <Button className="h-7 w-7 rounded-lg" variant="destructive"
-                   onClick={() => increaseProductQuantity(product.id)}>
-                     <ChevronRightIcon />
-                   </Button>
-                </div>
-             </div>
-            </div>
-            <Button className="h-7 w-7 rounded-lg" variant="outline" 
-            onClick={() => removeProduct(product.id)}>
-               <TrashIcon/>
+const CartProductItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity, increaseProductQuantity, removeProduct } =
+    useContext(CartContext);
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3">
+        <div className="relative h-20 w-20 rounded-xl bg-gray-100">
+          <Image src={product.imageUrl} alt={product.name} fill />
+        </div>
+        <div className="space-y-1">
+          <p className="max-w-[90%] truncate text-ellipsis text-xs">
+            {product.name}
+          </p>
+          <p className="text-sm font-semibold">
+            {formatCurrency(product.price)}
+          </p>
+          <div className="flex items-center gap-1 text-center">
+            <Button
+              className="h-7 w-7 rounded-lg"
+              variant="outline"
+              onClick={() => decreaseProductQuantity(product.id)}
+            >
+              <ChevronLeftIcon />
             </Button>
-       </div>
-     );
-}
- 
+            <p className="w-6 text-xs">{product.quantity}</p>
+            <Button
+              className="h-7 w-7 rounded-lg"
+              variant="destructive"
+              onClick={() => increaseProductQuantity(product.id)}
+            >
+              <ChevronRightIcon />
+            </Button>
+          </div>
+        </div>
+      </div>
+      <Button
+        className="h-7 w-7 rounded-lg"
+        variant="outline"
+        onClick={() => removeProduct(product.id)}
+      >
+        <TrashIcon />
+      </Button>
+    </div>
+  );
+};
+
 export default CartProductItem;
