1c694e1451ffd74f7e02d6df3a62e2538c87f4f4
gil-sousa2180 <gilvani.ss@hotmail.com>
2025-03-07 20:40:06 -0300

    feat: ensure user is redirected with cpf after order creation

--- src/app/[slug]/orders/page.tsx
+++ src/app/[slug]/orders/page.tsx
@@ -16,6 +16,10 @@ const OrdersPage = async ({ searchParams }: OrdersPageProps) => {
     return <CpfForm />;
   }
   const orders = await db.order.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+
     where: {
       customerCpf: removeCpfPunctuation(cpf),
     },
