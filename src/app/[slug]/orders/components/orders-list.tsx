4236237ddcc8a52c2dbcc52a290179204335aa6b
gil-sousa2180 <gilvani.ss@hotmail.com>
2025-03-07 22:53:41 -0300

    feat: ensure all orders and back buttons work as expected

--- src/app/[slug]/orders/components/orders-list.tsx
+++ src/app/[slug]/orders/components/orders-list.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
@@ -5,6 +7,7 @@ import { formatCurrency } from "@/format-currency";
 import { OrderStatus, Prisma } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface OrdersListProps {
   orders: Array<
@@ -32,9 +35,17 @@ const getStatusLabel = (status: OrderStatus) => {
   return "";
 };
 const OrdersList = ({ orders }: OrdersListProps) => {
+  const router = useRouter();
+  const handleBackClick = () => router.back();
+
   return (
     <div className="space-y-6 p-6">
-      <Button size="icon" variant="secondary" className="rounded-full">
+      <Button
+        size="icon"
+        variant="secondary"
+        className="rounded-full"
+        onClick={handleBackClick}
+      >
         <ChevronLeftIcon />
       </Button>
       <div className="flex items-center gap-3">
